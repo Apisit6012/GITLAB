e44ca6c4d3df9318c442c899e5c69b6d01347b7a
Apisit6012 <oomsin0810101520@gmail.com>
2021-09-16 21:39:05 +0700

    19A commits

--- bloodgroup.c
+++ bloodgroup.c
@@ -5,11 +5,11 @@ void main()
   int i;
   do
   {
-    printf("aWhat is your Name? Aomsin");
+    printf("What is your Name? Aomsin");
     scanf("%s", Name);
     do
     {
-      printf("aWhat is your blood group ^^[A, B, AB, or O]? ");
+      printf("aWhat is your blood group [A, B, AB, or O]? ");
       scanf("%s", G);
 
       if (strcmp(G, "A") !=0  &&
